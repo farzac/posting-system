59cb73b0501da6d672bbed9473197c1c48a4a703
farzac <zaccantte@gmail.com>
2021-04-02 01:27:54 -0300

    Add configutarion Azure in Frontend

--- frontend/src/main/environments.js
+++ frontend/src/main/environments.js
@@ -2,7 +2,11 @@ const dev = {
     "backendUrl":"http://localhost:9999"
 }
 
-const config = dev;
+const azure = {
+    "backendUrl":"https://posting-ms.azurewebsites.net/"
+}
+
+const config = azure;
 
 export default {
     ...config
